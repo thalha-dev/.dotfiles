3cac8e912abed13fbd3082e01a08e1eabed2395c
thalha-dev <dev.thalha@gmail.com>
2022-08-18 13:51:59 +0530

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require("wezterm")
+return {
+	font = wezterm.font("JetBrains Mono"),
+	color_scheme = "Batman",
+	-- Batman
+	-- Catppuccin Mocha
+	-- Catch Me If You Can (terminal.sexy)
+	-- ayu
+	-- Icy Dark (base16)
+	-- SynthwaveAlpha
+	-- Scarlet Protocol
+	-- SOS (terminal.sexy)
+	-- Sakura
+	-- Omni (Gogh)
+	-- mono-cyan (Gogh)
+	-- Mono Theme (terminal.sexy)
+	-- Mellow Purple (base16)
+	-- Lavandula
+	-- Matrix (terminal.sexy)
+	-- Laser
+	-- lovelace
+	-- matrix
+	window_background_opacity = 0.8,
+	--[[ window_background_image = "/home/thalha/.config/wezterm/wallhaven-j3m8y5_1366x768.png", ]]
+	enable_tab_bar = false,
+
+	window_padding = {
+		left = 0,
+		right = 0,
+		--[[ top = 0, ]]
+		bottom = 0,
+	},
+}
