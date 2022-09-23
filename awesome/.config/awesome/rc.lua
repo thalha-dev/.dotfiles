db4a40d5e2c7c7cf1df15958cf16fe2056103ded
thalha-dev <dev.thalha@gmail.com>
2022-09-23 14:28:33 +0530

    set transparent-clipping=true in picom
    remap slock to mod+: in awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -112,60 +112,9 @@ else
     })
 end
 
-
---[[ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, ]]
---[[                                      menu = mymainmenu }) ]]
-
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
 
--- Keyboard map indicator and switcher
---[[ mykeyboardlayout = awful.widget.keyboardlayout() ]]
---[[]]
---[[ -- {{{ Wibar ]]
---[[ -- Create a textclock widget ]]
---[[ mytextclock = wibox.widget.textclock() ]]
---[[]]
---[[ -- Create a wibox for each screen and add it ]]
---[[ local taglist_buttons = gears.table.join( ]]
---[[                     awful.button({ }, 1, function(t) t:view_only() end), ]]
---[[                     awful.button({ modkey }, 1, function(t) ]]
---[[                                               if client.focus then ]]
---[[                                                   client.focus:move_to_tag(t) ]]
---[[                                               end ]]
---[[                                           end), ]]
---[[                     awful.button({ }, 3, awful.tag.viewtoggle), ]]
---[[                     awful.button({ modkey }, 3, function(t) ]]
---[[                                               if client.focus then ]]
---[[                                                   client.focus:toggle_tag(t) ]]
---[[                                               end ]]
---[[                                           end), ]]
---[[                     awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end), ]]
---[[                     awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end) ]]
---[[                 ) ]]
---[[]]
---[[ local tasklist_buttons = gears.table.join( ]]
---[[                      awful.button({ }, 1, function (c) ]]
---[[                                               if c == client.focus then ]]
---[[                                                   c.minimized = true ]]
---[[                                               else ]]
---[[                                                   c:emit_signal( ]]
---[[                                                       "request::activate", ]]
---[[                                                       "tasklist", ]]
---[[                                                       {raise = true} ]]
---[[                                                   ) ]]
---[[                                               end ]]
---[[                                           end), ]]
---[[                      awful.button({ }, 3, function() ]]
---[[                                               awful.menu.client_list({ theme = { width = 250 } }) ]]
---[[                                           end), ]]
---[[                      awful.button({ }, 4, function () ]]
---[[                                               awful.client.focus.byidx(1) ]]
---[[                                           end), ]]
---[[                      awful.button({ }, 5, function () ]]
---[[                                               awful.client.focus.byidx(-1) ]]
---[[                                           end)) ]]
 
 local function set_wallpaper(s)
     -- Wallpaper
@@ -292,7 +241,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"    }, "Print", function() awful.util.spawn("gnome-screenshot -i") end,
         {description = "Gnome screenshot gui", group = "screenshots"}),
 
-    awful.key({ modkey,     "Tab"  },  "l", function() awful.util.spawn("slock") end,
+    awful.key({ modkey,     "Shift"  },  "semicolon", function() awful.util.spawn("slock") end,
         {description = "Screen Lock", group = "launcher"}),
 
     awful.key({ modkey,            },  "F11", function() awful.util.spawn("picom --experimental-backends -b") end,
