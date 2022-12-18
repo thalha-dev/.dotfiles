d01cc49b88e95d3231fd7356fd0d21f3a3c59e66
thalha-dev <dev.thalha@gmail.com>
2022-12-18 10:02:04 +0530

    fix css highlighting in treesitter nvim

--- nvim/.config/nvim/lua/thalha/treesitter.lua
+++ nvim/.config/nvim/lua/thalha/treesitter.lua
@@ -1,30 +1,30 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	-- ensure_installed = {"c", "lua", "java", "haskell", "javascript", "json", "regex", "sql", "toml", "vim", "yaml","vue", "typescript","bash", "comment", "css", "dockerfile","fish","go", "html","python","cpp","tsx"}
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
+  ensure_installed = "all", -- one of "all" or a list of languages
+  -- ensure_installed = {"c", "lua", "java", "haskell", "javascript", "json", "regex", "sql", "toml", "vim", "yaml","vue", "typescript","bash", "comment", "css", "dockerfile","fish","go", "html","python","cpp","tsx"}
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = {}, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
 })
