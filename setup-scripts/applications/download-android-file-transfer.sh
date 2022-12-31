58ba2e59e9d93df8cd622a99d168acdb2c11f333
thalha-dev <dev.thalha@gmail.com>
2022-12-31 10:51:05 +0530

    add setup script for android-file-transefer

---
+++ setup-scripts/applications/download-android-file-transfer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+cd ~/applications
+
+curl -JOL https://github.com/whoozle/android-file-transfer-linux/releases/download/continuous/Android_File_Transfer_For_Linux-9967dec-x86_64.AppImage
+
+sudo chmod +x Android_File_Transfer_For_Linux-9967dec-x86_64.AppImage
