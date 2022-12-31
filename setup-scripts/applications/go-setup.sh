8b27093de01707bd0b984d9cc3f7b7da1ebb2fea
thalha-dev <dev.thalha@gmail.com>
2022-12-31 10:17:59 +0530

    resolve missed curl in go-setup

--- setup-scripts/applications/go-setup.sh
+++ setup-scripts/applications/go-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mkdir -p ~/source-installed
+cd ~/source-installed
 
- rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+curl -JOL https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
+
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
