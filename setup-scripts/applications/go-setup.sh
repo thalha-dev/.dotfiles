14c14765262673c7972046b7b54ea9f2d247f19e
thalha-dev <dev.thalha@gmail.com>
2023-01-02 19:17:54 +0530

    fix permission issue in go-setup script

--- setup-scripts/applications/go-setup.sh
+++ setup-scripts/applications/go-setup.sh
@@ -5,4 +5,4 @@ cd ~/source-installed
 
 curl -JOL https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
