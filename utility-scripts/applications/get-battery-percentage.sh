bd27507ac83055b133f87604af00970353670eee
thalha-dev <dev.thalha@gmail.com>
2023-04-12 19:39:07 +0530

    add status indicator to battery in polybar

--- utility-scripts/applications/get-battery-percentage.sh
+++ utility-scripts/applications/get-battery-percentage.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-acpi -b | grep -P -o '[0-9]+(?=%)'
+percentage=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+
+ChargingStatus=$(acpi | awk -F ',' '{print $1}' | awk -F ':' '{print $2}' | sed "s/[[:blank:]$SPACES]//g")
+
+if [[ "$ChargingStatus" == "Discharging" ]]; then
+    echo "${percentage}"
+else
+    echo "▒▒ ${percentage}"
+fi
