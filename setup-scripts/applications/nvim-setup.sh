4c337fe058cfe640fa19a8fab0cda0f74c04285a
thalha-dev <dev.thalha@gmail.com>
2023-04-30 15:03:40 +0530

    update nvim install script

--- setup-scripts/applications/nvim-setup.sh
+++ setup-scripts/applications/nvim-setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+url="https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz"
+extract_dir="$HOME/source-installed/nvim/stable"
+nvim_bin="$HOME/.local/bin/nvim"
 
-#  _   _ _____ _____     _____ __  __
-# | \ | | ____/ _ \ \   / /_ _|  \/  |
-# |  \| |  _|| | | \ \ / / | || |\/| |
-# | |\  | |__| |_| |\ V /  | || |  | |
-# |_| \_|_____\___/  \_/  |___|_|  |_|
-#
-echo ""
-echo "NEOVIM !!!"
-echo ""
-mkdir -p ~/source-installed
-cd ~/source-installed
-curl -JOL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb
+# Remove existing files in the extract directory
+if [[ -d "$extract_dir" ]]; then
+	echo "Removing existing files in $extract_dir..."
+	rm -rf "$extract_dir"
+fi
+
+# Remove symlink if it already exists
+if [ -L "$nvim_bin" ]; then
+	rm "$nvim_bin"
+fi
+
+mkdir -p "$extract_dir"
+
+curl -L "$url" | tar -xz -C "$extract_dir" --strip-components=1
+
+# Create symlink to nvim executable
+ln -s "$extract_dir/bin/nvim" "$nvim_bin"
