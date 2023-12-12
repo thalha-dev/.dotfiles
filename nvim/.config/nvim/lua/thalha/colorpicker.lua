b2625974118228e1c1fb00c89b2856b7ad9002f3
thalha-dev <dev.thalha@gmail.com>
2023-12-12 22:25:40 +0530

    trash old nvim config

--- nvim/.config/nvim/lua/thalha/colorpicker.lua
+++
@@ -1,4 +0,0 @@
-local status_ok, colorpicker = pcall(require, "color-picker")
-if not status_ok then
-  return
-end
--- nvim/.config/nvim/lua/thalha/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-    return
-end
-
-require "thalha.lsp.configs"
-require("thalha.lsp.handlers").setup()
-require "thalha.lsp.null-ls"
